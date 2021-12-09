4c5592e05750811ba0aae87b4d7f546023494839
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-12-09 04:18:56 +0000

    Day 8 part 1

--- src/aoc2021/d8/mod.rs
+++ src/aoc2021/d8/mod.rs
@@ -2,4 +2,18 @@ use crate::utils;
 
 pub fn solve(input1: String, _: String, _: &[String]) {
     let lines = utils::read_file_lines(&input1);
+
+    let outputs = lines.iter()
+        .filter_map(|l| l.split(" | ").nth(1))
+        .flat_map(|l| l.split(" "))
+        .collect::<Vec<_>>();
+
+    let outputs_1478 = outputs.iter()
+        .filter(|o| match o.len() {
+            2 | 3 | 4 | 7 => true,
+            _ => false,
+        })
+        .count();
+
+    println!("{:?}", outputs_1478);
 }
