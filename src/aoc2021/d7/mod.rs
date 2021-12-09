053603fd6acb09f55393395ea3120626adf5e280
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-12-09 04:06:59 +0000

    Day 7 part 2

--- src/aoc2021/d7/mod.rs
+++ src/aoc2021/d7/mod.rs
@@ -7,8 +7,6 @@ pub fn solve(input1: String, _: String, _: &[String]) {
         .map(|i| i.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
     
-    let count = start.iter().count();
-    let sum = start.iter().sum::<usize>();
     let max = start.iter().max().unwrap();
     let mut track: Vec<usize> = vec![0; *max as usize + 1];
 
@@ -19,20 +17,35 @@ pub fn solve(input1: String, _: String, _: &[String]) {
 
     let mut lowest_pos = 0;
     let mut lowest_cost = 999999999;
+    let mut lowest_pos_2 = 0;
+    let mut lowest_cost_2 = 999999999;
     for i in 0..=*max {
         let mut cost = 0;
+        let mut cost_2 = 0;
         for j in 0..i {
             cost += (i-j) * track[j];
+            cost_2 += travel_cost_2(i-j) * track[j];
         }
         for j in (i + 1)..=*max {
             cost += (j-i) * track[j];
+            cost_2 += travel_cost_2(j-i) * track[j];
         }
 
         if cost < lowest_cost {
             lowest_cost = cost;
             lowest_pos = i;
         }
+
+        if cost_2 < lowest_cost_2 {
+            lowest_cost_2 = cost_2;
+            lowest_pos_2 = i;
+        }
     }
 
     println!("Lowest cost at position {} with {}", lowest_pos, lowest_cost);
+    println!("Lowest cost 2 at position {} with {}", lowest_pos_2, lowest_cost_2);
+}
+
+fn travel_cost_2(dist: usize) -> usize {
+    ((dist * dist) + dist) / 2
 }
