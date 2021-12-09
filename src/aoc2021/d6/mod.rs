4eefb1d30019dcfc8ffe884ed07c44cc0c0e2002
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-12-09 03:34:09 +0000

    Day 6 part 1 done

--- src/aoc2021/d6/mod.rs
+++ src/aoc2021/d6/mod.rs
@@ -1,7 +1,26 @@
 use crate::utils;
 
-type VentVecs = Vec<((usize, usize), (usize, usize))>;
-
 pub fn solve(input1: String, _: String, _: &[String]) {
     let lines = utils::read_file_lines(&input1);
+
+    let mut track = vec![0; 9];
+    
+    lines.get(0).unwrap()
+        .split(",")
+        .map(|n| n.parse::<usize>().unwrap())
+        .for_each(|n| track[n] += 1);
+
+    println!("{:?}", track);
+
+    let mut birthers: i32;
+    for _ in 1..=80 {
+        birthers = track[0];
+        for j in 1..=8 {
+            track[j-1] = track[j];
+        }
+        track[8] = birthers;
+        track[6] += birthers;
+    }
+    println!("Day 80 fish: {}", track.iter().sum::<i32>());
+
 }
