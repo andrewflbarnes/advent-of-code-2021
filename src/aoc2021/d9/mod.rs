1c0d7017fc5bd62638293c1e53a2cd2bcb165f0e
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-12-09 06:45:21 +0000

    Day 9 part 2

--- src/aoc2021/d9/mod.rs
+++ src/aoc2021/d9/mod.rs
@@ -5,25 +5,33 @@ pub fn solve(input1: String, _: String, _: &[String]) {
 
     let cave = lines.iter()
         .map(|l| l.chars()
-            .map(|c| c.to_string().parse::<u32>().unwrap())
+            .map(|c| c.to_string().parse::<i32>().unwrap())
             .collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
     // println!("{:?}", cave);
 
     let lps = find_low_points(&cave);
-    println!("Low points: {:?}", lps);
+    // println!("Low points: {:?}", lps);
 
     let result = lps.iter()
         .map(|p| cave[p.0][p.1] + 1)
-        .sum::<u32>();
+        .sum::<i32>();
     println!("Low point sum: {:?}", result);
 
-    let basins = find_basins(&cave, &lps);
-    println!("{:?}", basins);
+    let mut basins = find_basins(&cave, &lps);
+    // println!("{:?}", basins);
+
+    basins.sort();
+
+    let mut basin_sizes = basins.iter().map(|b| b.1.len()).collect::<Vec<_>>();
+    basin_sizes.sort();
+    let result = basin_sizes.iter().rev().take(3).product::<usize>();
+    println!("Basin product: {}", result);
+
 }
 
-fn find_low_points(cave: &Vec<Vec<u32>>) -> Vec<(usize, usize)> {
+fn find_low_points(cave: &Vec<Vec<i32>>) -> Vec<(usize, usize)> {
     let mut lps = vec![];
 
     let rowmax = cave.len() - 1;
@@ -44,8 +52,48 @@ fn find_low_points(cave: &Vec<Vec<u32>>) -> Vec<(usize, usize)> {
     lps
 }
 
-fn find_basins(cave: &Vec<Vec<u32>>, low_points: &Vec<(usize, usize)>) -> Vec<(usize, usize, u32)> {
+fn find_basins(cave: &Vec<Vec<i32>>, low_points: &Vec<(usize, usize)>) -> Vec<((usize, usize), Vec<(usize, usize)>)> {
     let mut basins = vec![];
 
+    low_points.iter()
+        .for_each(|lp| basins.push((lp.clone(), basin_area(&cave, &lp))));
+
     basins
+}
+
+fn basin_area(cave: &Vec<Vec<i32>>, low_point: &(usize, usize)) -> Vec<(usize, usize)> {
+    basin_area_acc(&cave, low_point, vec![low_point.clone()])
+}
+
+fn basin_area_acc(cave: &Vec<Vec<i32>>, point: &(usize, usize), mut tracker: Vec<(usize, usize)>) -> Vec<(usize, usize)> {
+    let rowmax = cave.len() - 1;
+    let colmax = cave[0].len() - 1;
+    let row = point.0;
+    let col = point.1;
+
+    if row != 0 && !tracker.contains(&(row - 1, col)) && cave[row - 1][col] != 9 {
+        let next = (row - 1, col);
+        tracker.push(next);
+        tracker = basin_area_acc(&cave, &next, tracker);
+    }
+
+    if row != rowmax && !tracker.contains(&(row + 1, col)) && cave[row + 1][col] != 9 {
+        let next = (row + 1, col);
+        tracker.push(next);
+        tracker = basin_area_acc(&cave, &next, tracker);
+    }
+
+    if col != 0 && !tracker.contains(&(row , col - 1)) && cave[row][col - 1] != 9 {
+        let next = (row , col - 1);
+        tracker.push(next);
+        tracker = basin_area_acc(&cave, &next, tracker);
+    }
+
+    if col != colmax && !tracker.contains(&(row , col + 1)) && cave[row][col + 1] != 9 {
+        let next = (row , col + 1);
+        tracker.push(next);
+        tracker = basin_area_acc(&cave, &next, tracker);
+    }
+
+    tracker
 }
